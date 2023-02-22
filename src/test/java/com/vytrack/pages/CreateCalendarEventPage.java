0fec9474ef755383bd200d4c73513507d69e9d75
ihuseyn <ihuseyn@yahoo.com>
2023-02-22 07:06:16 -0800

    cuks

---
+++ src/test/java/com/vytrack/pages/CreateCalendarEventPage.java
@@ -0,0 +1,58 @@
+package com.vytrack.pages;
+
+import com.vytrack.utils.BrowserUtils;
+import com.vytrack.utils.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.awt.*;
+
+public class CreateCalendarEventPage extends BasePage{
+
+   @FindBy(css = "[title='Create Calendar event']")
+    private WebElement crateCalendarEventButton;
+
+   @FindBy(name = "oro_calendar_event_form[title]")
+    private WebElement titleInputBox;
+
+   @FindBy(id = "tinymce")
+    private WebElement descriptionInputBox;
+
+   public void clickCreateCalendarEvent(){
+       BrowserUtils.clickOnElement(crateCalendarEventButton);
+   }
+
+
+
+
+   public void enterTitle(String txt){
+       BrowserUtils.enterTxt(titleInputBox,txt);
+
+   }
+   public void enterDescription(String txt){
+       WebDriverWait wait=new WebDriverWait(Driver.getDriver(),30);
+       // defaultContent() exits all frames
+
+       Driver.getDriver().switchTo().defaultContent();
+       // wait frame and switch to it
+       wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(0));
+       //enter txt
+       descriptionInputBox.sendKeys(txt);
+  //  BrowserUtils.enterTxt(descriptionInputBox,txt);
+       // exit from the frame
+       Driver.getDriver().switchTo().defaultContent();
+
+   }
+
+   public String getDateFromGeneralInfo(String parameterName ){
+       WebDriverWait wait=new WebDriverWait(Driver.getDriver(),10);
+       String xpath="//label[text()='"+parameterName+"']/../div/div";
+       WebElement element=wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
+       return element.getText().trim();
+   }
+
+}
---
+++ src/test/java/com/vytrack/step_definitions/CreateCalendarEventStepDefinitions.java
@@ -0,0 +1,64 @@
+package com.vytrack.step_definitions;
+
+import com.vytrack.pages.BasePage;
+import com.vytrack.pages.CreateCalendarEventPage;
+import com.vytrack.pages.LoginPage;
+import com.vytrack.utils.BrowserUtils;
+import com.vytrack.utils.ConfigurationReader;
+import com.vytrack.utils.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+import java.util.Map;
+import java.util.function.LongFunction;
+
+public class CreateCalendarEventStepDefinitions {
+
+    CreateCalendarEventPage createCalendarEventPage=new CreateCalendarEventPage();
+    LoginPage loginPage=new LoginPage();
+
+    @Given("user is on the login page")
+    public void user_is_on_the_login_page() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+      //  loginPage.login(ConfigurationReader.getProperty("storemanager.username="));
+    }
+
+    @Given("user clicks on create calendar event button")
+    public void user_clicks_on_create_calendar_event_button() {
+        BrowserUtils.wait(5);
+        createCalendarEventPage.clickCreateCalendarEvent();
+
+    }
+
+    /**
+      | Title | B20 Graduation Party |
+      | Description | All B20 friends are invited for this party! |    */
+
+    @When("user adds new calendar event information")
+    public void user_adds_new_calendar_event_information(Map<String, String> data) {
+        data.forEach( (k,v)-> System.out.println("key: "+k+" "+",-> Value: "+v));
+        String title=data.get("Title");
+        String description=data.get("Description");
+
+        createCalendarEventPage.enterTitle(title);
+        createCalendarEventPage.enterDescription(description);
+        BrowserUtils.wait(4);
+
+    }
+
+
+    @Then("user verifies that new calendar event is displayed:")
+    public void user_verifies_that_new_calendar_event_is_displayed(Map<String, String> data) {
+        BrowserUtils.wait(4);
+        String title=data.get("Title");
+        String description=data.get("Description");
+
+        Assert.assertEquals("Title Didn't match",title,createCalendarEventPage.getDateFromGeneralInfo("Title"));
+        Assert.assertEquals("Description Not matching",description,createCalendarEventPage.getDateFromGeneralInfo("Description"));
+
+    }
+
+
+}
